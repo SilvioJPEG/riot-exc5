1e7d33d29776c51e76f764cdc1f697fdae486d81
alex <lyas-al@yandex.ru>
2023-02-26 18:33:06 +0300

    version 2

--- main.c
+++ main.c
@@ -3,111 +3,101 @@
 #include "board.h"
 #include "periph/gpio.h"
 #include "xtimer.h"
+#include "irq.h"
 
-//TODO: replace pins
-#define BTN_PIN                 GPIO_PIN(PORT_C, 0)
+#define toggleButton GPIO_PIN(PORT_C, 0)
 
-#define PEDESTRIAN_LED_RED      GPIO_PIN(PORT_D, 2)
-#define PEDESTRIAN_LED_GREEN    GPIO_PIN(PORT_D, 3)
+// Cars traffic lights
+#define mainRed GPIO_PIN(PORT_B, 0)
+#define mainYellow GPIO_PIN(PORT_B, 1)
+#define mainGreen GPIO_PIN(PORT_B, 2)
 
-#define CAR_LED_GREEN           GPIO_PIN(PORT_B, 2)
-#define CAR_LED_YELLOW          GPIO_PIN(PORT_B, 1)
-#define CAR_LED_RED             GPIO_PIN(PORT_B, 0)
+// Pedestrians traffic light
+#define pedRed GPIO_PIN(PORT_D, 2)
+#define pedGreen GPIO_PIN(PORT_D, 3)
 
-const uint32_t car_traffic_time = 60000000; // 60 sec
-const uint32_t pedestrian_time = 10000000; // 10 sec
+#define CARS_MOVING_TIME 30
+#define PEDS_MOVING_TIME 5
+#define TOGGLE_TIMEOUT 10
 
-enum light_state {
-    green,  // 0
-    yellow, // 1
-    red     // 2
-};
-
-void changePedastrianLight(enum light_state pedastrian_light_state) {
-    gpio_clear(PEDESTRIAN_LED_GREEN);
-    gpio_clear(PEDESTRIAN_LED_RED);
-    switch (pedastrian_light_state) {
-    case green:
-        gpio_set(PEDESTRIAN_LED_GREEN);
-        break;
-    case red:
-        gpio_set(PEDESTRIAN_LED_RED);
-        break;
-    default:
-        break;
+void toggleToPedestrain(void) {
+    // Blink green
+    for (int i = 0; i < 3; i++) {
+        gpio_clear(mainGreen);
+        xtimer_msleep(300);
+        gpio_set(mainGreen);
+        xtimer_msleep(300);
     }
+
+    // Turn on yellow
+    gpio_set(mainYellow);
+    gpio_clear(mainGreen);
+
+    // Turn on red for cars and turn green for peds
+    xtimer_msleep(1000);
+    gpio_clear(mainYellow);
+    gpio_set(mainRed);
+    gpio_clear(pedRed);
+    gpio_set(pedGreen);
 }
 
-void changeCarLight(enum light_state car_light_state) {
-    gpio_clear(CAR_LED_GREEN);
-    gpio_clear(CAR_LED_YELLOW);
-    gpio_clear(CAR_LED_RED);
-    switch (car_light_state) {
-    case green:
-        gpio_set(CAR_LED_GREEN);
-        break;
-    case yellow:
-        gpio_set(CAR_LED_YELLOW);
-        break;
-    case red:
-        gpio_set(CAR_LED_RED);
-        break;
-    default:
-        break;
+void toggleToCars(void) {
+    for (int i = 0; i < 3; i++) {
+        gpio_clear(pedGreen);
+        xtimer_msleep(300);
+        gpio_set(pedGreen);
+        xtimer_msleep(300);
     }
+    gpio_clear(pedGreen);
+    gpio_set(pedRed);
+
+    xtimer_msleep(500);
+    gpio_clear(mainRed);
+    gpio_set(mainGreen);
+}
+volatile bool carsMoving = true;
+volatile uint8_t timeLeft = CARS_MOVING_TIME;
+
+void button_handler(void* arg) {
+    if (timeLeft > TOGGLE_TIMEOUT && carsMoving) {
+        timeLeft = TOGGLE_TIMEOUT;
+    }
+    (void) arg;
 }
 
 int main(void) {
-    uint32_t current_time,
-        car_traffic_start, car_delta,
-        pedestrian_start, pedastrian_delta;
-    car_traffic_start = xtimer_now_usec();
-    // bool car_light_blink = false;
-    bool pedastrian_light_blink = false;
-    /**
-     * 0 = green;
-     * 1 = yellow;
-     * 2 = red;
-    */
-    enum light_state car_light_state = green;
-    enum light_state pedastrian_light_state = green;
-    gpio_init(CAR_LED_GREEN, GPIO_OUT);
-    gpio_init(CAR_LED_YELLOW, GPIO_OUT);
-    gpio_init(CAR_LED_RED, GPIO_OUT);
+    gpio_init_int(toggleButton,
+        GPIO_IN,
+        GPIO_BOTH,
+        button_handler, NULL
+    );
+    gpio_init(mainRed, GPIO_OUT);
+    gpio_init(mainYellow, GPIO_OUT);
+    gpio_init(mainGreen, GPIO_OUT);
+    gpio_init(pedRed, GPIO_OUT);
+    gpio_init(pedGreen, GPIO_OUT);
+    gpio_clear(mainRed);
+    gpio_clear(mainYellow);
+    gpio_set(mainGreen);
+    gpio_clear(pedGreen);
+    gpio_set(pedRed);
+    gpio_irq_enable(toggleButton);
 
-    gpio_init(PEDESTRIAN_LED_RED, GPIO_OUT);
-    gpio_init(PEDESTRIAN_LED_GREEN, GPIO_OUT);
-    gpio_init(BTN_PIN, GPIO_IN);
-    while (1) {
-        current_time = xtimer_now_usec();
-        car_delta = current_time - car_traffic_start;
+    while (true) {
+        xtimer_msleep(1000);
+        timeLeft--;
 
-
-        // время пешеходов
-        if (car_delta >= car_traffic_time) {
-            car_light_state = red;
-            pedastrian_light_state = green;
-            pedestrian_start = xtimer_now_usec();
-            pedastrian_delta = 0U;
-            while (pedastrian_delta < pedestrian_time) {
-                current_time = xtimer_now_usec();
-                pedastrian_delta = current_time - pedestrian_start;
-                if (pedastrian_delta > pedestrian_time / 2) {
-                    pedastrian_light_blink = true;
-                    car_light_state = yellow;
-                }
-                changePedastrianLight(pedastrian_light_state);
-                if (pedastrian_light_blink) xtimer_usleep(100000);
+        if (timeLeft == 0) {
+            if (carsMoving) {
+                carsMoving = false;
+                toggleToPedestrain();
+                timeLeft = PEDS_MOVING_TIME;
             }
-            car_traffic_start = xtimer_now_usec();
+            else {
+                carsMoving = true;
+                toggleToCars();
+                timeLeft = CARS_MOVING_TIME;
+            }
         }
-
-
-        if (car_delta < car_traffic_time) {
-            car_light_state = green;
-        }
-
-        changeCarLight(car_light_state);
     }
-    return 0;
 }
