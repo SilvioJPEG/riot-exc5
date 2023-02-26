f8a5136604e2b241646aec97f17bfb8fa4d9ba1b
alex <lyas-al@yandex.ru>
2023-02-26 20:40:46 +0300

    define name change

--- main.c
+++ main.c
@@ -5,16 +5,14 @@
 #include "xtimer.h"
 #include "irq.h"
 
-#define toggleButton GPIO_PIN(PORT_C, 0)
-
-// Cars traffic lights
-#define mainRed GPIO_PIN(PORT_B, 0)
-#define mainYellow GPIO_PIN(PORT_B, 1)
-#define mainGreen GPIO_PIN(PORT_B, 2)
-
+#define BTN_PIN                 GPIO_PIN(PORT_C, 0)
 // Pedestrians traffic light
-#define pedRed GPIO_PIN(PORT_D, 2)
-#define pedGreen GPIO_PIN(PORT_D, 3)
+#define PEDESTRIAN_LED_RED      GPIO_PIN(PORT_D, 2)
+#define PEDESTRIAN_LED_GREEN    GPIO_PIN(PORT_D, 3)
+// Cars traffic lights
+#define CAR_LED_GREEN           GPIO_PIN(PORT_B, 2)
+#define CAR_LED_YELLOW          GPIO_PIN(PORT_B, 1)
+#define CAR_LED_RED             GPIO_PIN(PORT_B, 0)
 
 #define CARS_MOVING_TIME 30
 #define PEDS_MOVING_TIME 5
@@ -23,38 +21,36 @@
 void toggleToPedestrain(void) {
     // Blink green
     for (int i = 0; i < 3; i++) {
-        gpio_clear(mainGreen);
+        gpio_clear(CAR_LED_GREEN);
         xtimer_msleep(300);
-        gpio_set(mainGreen);
+        gpio_set(CAR_LED_GREEN);
         xtimer_msleep(300);
     }
-
     // Turn on yellow
-    gpio_set(mainYellow);
-    gpio_clear(mainGreen);
+    gpio_set(CAR_LED_YELLOW);
+    gpio_clear(CAR_LED_GREEN);
 
     // Turn on red for cars and turn green for peds
     xtimer_msleep(1000);
-    gpio_clear(mainYellow);
-    gpio_set(mainRed);
-    gpio_clear(pedRed);
-    gpio_set(pedGreen);
+    gpio_clear(CAR_LED_YELLOW);
+    gpio_set(CAR_LED_RED);
+    gpio_clear(PEDESTRIAN_LED_RED);
+    gpio_set(PEDESTRIAN_LED_GREEN);
 }
 
 void toggleToCars(void) {
     for (int i = 0; i < 3; i++) {
-        gpio_clear(pedGreen);
+        gpio_clear(PEDESTRIAN_LED_GREEN);
         xtimer_msleep(300);
-        gpio_set(pedGreen);
+        gpio_set(PEDESTRIAN_LED_GREEN);
         xtimer_msleep(300);
     }
-    gpio_clear(pedGreen);
-    gpio_set(pedRed);
-
-    xtimer_msleep(500);
-    gpio_clear(mainRed);
-    gpio_set(mainGreen);
+    gpio_clear(PEDESTRIAN_LED_GREEN);
+    gpio_set(PEDESTRIAN_LED_RED);
+    gpio_clear(CAR_LED_RED);
+    gpio_set(CAR_LED_GREEN);
 }
+
 volatile bool carsMoving = true;
 volatile uint8_t timeLeft = CARS_MOVING_TIME;
 
@@ -62,31 +58,31 @@ void button_handler(void* arg) {
     if (timeLeft > TOGGLE_TIMEOUT && carsMoving) {
         timeLeft = TOGGLE_TIMEOUT;
     }
-    (void) arg;
+    (void)arg;
 }
 
 int main(void) {
-    gpio_init_int(toggleButton,
+    int res = gpio_init_int(BTN_PIN,
         GPIO_IN,
-        GPIO_BOTH,
+        GPIO_FALLING,
         button_handler, NULL
     );
-    gpio_init(mainRed, GPIO_OUT);
-    gpio_init(mainYellow, GPIO_OUT);
-    gpio_init(mainGreen, GPIO_OUT);
-    gpio_init(pedRed, GPIO_OUT);
-    gpio_init(pedGreen, GPIO_OUT);
-    gpio_clear(mainRed);
-    gpio_clear(mainYellow);
-    gpio_set(mainGreen);
-    gpio_clear(pedGreen);
-    gpio_set(pedRed);
-    gpio_irq_enable(toggleButton);
+    printf("Interupt result: %d\r\n", res);
+    gpio_init(CAR_LED_RED, GPIO_OUT);
+    gpio_init(CAR_LED_YELLOW, GPIO_OUT);
+    gpio_init(CAR_LED_GREEN, GPIO_OUT);
+    gpio_init(PEDESTRIAN_LED_RED, GPIO_OUT);
+    gpio_init(PEDESTRIAN_LED_GREEN, GPIO_OUT);
+    gpio_clear(CAR_LED_RED);
+    gpio_clear(CAR_LED_YELLOW);
+    gpio_set(CAR_LED_GREEN);
+    gpio_clear(PEDESTRIAN_LED_GREEN);
+    gpio_set(PEDESTRIAN_LED_RED);
+    gpio_irq_enable(BTN_PIN);
 
     while (true) {
         xtimer_msleep(1000);
         timeLeft--;
-
         if (timeLeft == 0) {
             if (carsMoving) {
                 carsMoving = false;
